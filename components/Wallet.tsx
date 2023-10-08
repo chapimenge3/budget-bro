fbcd4ec424c4b88724aaa33c8508a22cfd55e875
Chapi Menge <chapimenge3@gmail.com>
2023-10-09 00:51:28 +0300

    Wallet deletion added using dialog box as confirmation

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -1,18 +1,46 @@
-import React from 'react'
+'use client'
+import { useState } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
 import { WalletIcon } from '@heroicons/react/20/solid'
 import { Button } from './ui/button'
 import Link from 'next/link'
-
+import { deleteWallet } from '@/lib/wallet'
+import { Toaster } from './ui/toaster'
+import { useRouter } from 'next/navigation'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { useToast } from './ui/use-toast'
+import { redirect } from 'next/navigation'
 interface WalletProps {
-    id: number
+    id: string,
     name: string
     amount: number,
     currency: string,
+    initial_balance: number,
+    showButton?: boolean | true
 }
 
 
-export default function Wallet({ id, name, amount, currency }: WalletProps) {
+export default function Wallet({ id, name, amount, currency, initial_balance, showButton }: WalletProps) {
+    const [open, setOpen] = useState(false)
+    const { toast } = useToast();
+    const router = useRouter()
+
+    const calculatePercentage = (initial_balance: number, current_balance: number) => {
+        return ((current_balance - initial_balance) / initial_balance) * 100
+    }
+
+    const percentageChange = calculatePercentage(initial_balance, amount)
+
     return (
         <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
@@ -25,14 +53,61 @@ export default function Wallet({ id, name, amount, currency }: WalletProps) {
                 <div className="text-2xl font-bold">
                     {Intl.NumberFormat('en-US', { style: 'currency', currency: currency }).format(amount)}
                 </div>
-                {/* <p className="text-xs text-muted-foreground">
-                    +20.1% from last month
-                </p> */}
-                <Button className="mt-4">
-                    <Link href={`/wallet/${id}`}>
-                        View Wallet
-                    </Link>
-                </Button>
+                <p className="text-xs text-muted-foreground">
+                    {
+                        percentageChange >= 0 ?
+                            <span className="text-green-500">
+                                {percentageChange.toFixed(2)}% from your initial balance
+                            </span>
+                            :
+                            <span className="text-red-500">
+                                {percentageChange.toFixed(2)}% from your initial balance
+                            </span>
+                    }
+                </p>
+                {
+                    showButton &&
+                    <Button className="mt-4">
+                        <Link href={`/wallet/${id}`}>
+                            View Wallet
+                        </Link>
+                    </Button>
+                }
+                {/* delete wallet button */}
+                {
+                    !showButton &&
+                    <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <Button className='mt-4' variant="destructive">Delete</Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                                <AlertDialogDescription>
+                                    This action cannot be undone. This will permanently delete your
+                                    wallet and transaction under this wallet from our servers.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                <AlertDialogAction
+                                    onClick={async () => {
+                                        await deleteWallet(id)
+                                        setOpen(false)
+                                        toast({
+                                            title: "Wallet deleted",
+                                            description: "Your wallet has been deleted.",
+                                            variant: "destructive",
+                                            duration: 5000,
+                                        })
+                                        router.push('/dashboard')
+                                    }}
+                                >
+                                    Continue
+                                </AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>}
             </CardContent>
         </Card>
     )
