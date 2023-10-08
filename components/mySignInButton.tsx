c5c76314bbe61c878773645ae1461861b55d2368
Chapi Menge <chapimenge3@gmail.com>
2023-10-09 01:12:07 +0300

    fix: Build errors by removing unnecessary files

--- components/mySignInButton.tsx
+++ components/mySignInButton.tsx
@@ -5,7 +5,7 @@ import { signIn } from 'next-auth/react'
 export default function SignInButton() {
     return (
         <button
-            className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
+            className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
             onClick={() => signIn()}
         >
             Sign in
