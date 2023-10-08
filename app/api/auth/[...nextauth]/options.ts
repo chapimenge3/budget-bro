387014717c0c3154605d1f1a6ae8cf7c785bc857
Chapi Menge <chapimenge3@gmail.com>
2023-10-09 00:47:49 +0300

    cleanup api directory

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -4,47 +4,17 @@ import GoogleProvider from "next-auth/providers/google"
 
 import { createUser, getUserByEmail } from "@/lib/user"
 
+// use adapter to connect to database
+
 export const options: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    // CredentialsProvider({
-    //   name: "credentials",
-    //   credentials: {
-    //     username: {
-    //       label: "Username",
-    //       type: "text",
-    //       placeholder: "spring-studio",
-    //     },
-    //     password: {
-    //       label: "Password",
-    //       type: "password",
-    //     },
-    //   },
-    //   async authorize(credentials) {
-    //     // database call
-    //     if (
-    //       credentials?.username === "admin" &&
-    //       credentials?.password === "admin"
-    //     ) {
-    //       return {
-    //         name: "admin",
-    //         username: "admin",
-    //         id: "1234567890",
-    //       };
-    //     }
-    //     return null;
-    //   },
-    // }),
   ],
   events: {
-    createUser: async (message) => {
-      console.log("createUser", message)
-    },
     signIn: async (message) => {
-      console.log("signIn", message)
       const user = message.user
       if (user.email) {
         const dbUser = await getUserByEmail(user.email)
